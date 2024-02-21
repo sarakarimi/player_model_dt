c152b81d1c0e3ed169edb15565a090ecac18c6b5
sarakarimi <sarakarimi30@gmail.com>
2024-02-21 16:20:15 +0100

    readme

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from . import data, decision_transformers, envs, trained_models, utils
-
-
-__all__ = ["data", "decision_transformers", "envs", "trained_models", "utils"]
--- decision_transformers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import img_obs, vector_obs
-
-
-__all__ = ["img_obs", "vector_obs"]
