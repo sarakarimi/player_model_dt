8c43322e43b790975428418e5cbfc83c2ea5ab97
sarakarimi <sarakarimi30@gmail.com>
2025-01-13 17:23:04 +0100

    fix

--- envs/base_level_human_mgs.py
+++ envs/base_level_human_mgs.py
@@ -1,14 +1,14 @@
 import pygame
 
-from metalgridsolid.core.item import Camouflage
-from metalgridsolid.environment import MetalGridSolidEnv
-from metalgridsolid.core.enemy import PatrollingEnemy, StandingEnemy
-from metalgridsolid.core.agent import Agent
-from metalgridsolid.utils.utils import create_horizontal_limit, create_vertical_limit
+from envs.metalgridsolid.core.item import Camouflage
+from envs.metalgridsolid.environment import MetalGridSolidEnv
+from envs.metalgridsolid.core.enemy import PatrollingEnemy, StandingEnemy
+from envs.metalgridsolid.core.agent import Agent
+from envs.metalgridsolid.utils.utils import create_horizontal_limit, create_vertical_limit
 
 
-if __name__ == "__main__":
 
+def metal_grid_env():
     grid_size = (15, 15)
 
     # Define walls to create a the base level room
@@ -29,7 +29,6 @@ if __name__ == "__main__":
     walls += create_horizontal_limit(row=11, col_start=9, col_end=11)
     walls += create_vertical_limit(col=9, row_start=11, row_end=13)
 
-
     # Define vent paths
     vent_l = []
     vent_l += create_horizontal_limit(row=6, col_start=3, col_end=6)
@@ -41,14 +40,13 @@ if __name__ == "__main__":
     vent_r += create_vertical_limit(col=12, row_start=6, row_end=12)
     vent_r += create_horizontal_limit(row=12, col_start=9, col_end=12)
 
-
     # Define agent
     agent = Agent(position=(1, 1), direction=2)
 
     # Define enemies
     enemies = [PatrollingEnemy(position=(3, 1), direction=1),
                StandingEnemy(position=(10, 9), direction=3)]
-    
+
     # Define goal position
     goal = (13, 7)
 
@@ -60,10 +58,10 @@ if __name__ == "__main__":
 
     # Reward weigths
     rw_weights = {
-        'goal': 1.0,            # Standard weight for reaching the goal
-        'takedown': 0.0,        # No weight for enemies killed
-        'camouflage': 0.0       # No weight for using the camouflage
-}   
+        'goal': 1.0,  # Standard weight for reaching the goal
+        'takedown': 0.0,  # No weight for enemies killed
+        'camouflage': 0.0  # No weight for using the camouflage
+    }
     # Define max steps of the environment
     max_steps = 100
 
@@ -71,9 +69,8 @@ if __name__ == "__main__":
     observation_mode = 'image'
     render_mode = 'human'
 
-
     # Create the environment
-    env = MetalGridSolidEnv(agent=agent,
+    metal_env = MetalGridSolidEnv(agent=agent,
                             enemies=enemies,
                             vents=[vent_l, vent_r],
                             items=items,
@@ -85,6 +82,12 @@ if __name__ == "__main__":
                             observation_mode=observation_mode,
                             render_mode=render_mode,
                             max_steps=max_steps)
+
+    return metal_env
+
+if __name__ == "__main__":
+
+    env = metal_grid_env()
     obs, _ = env.reset()
 
     finish = False
